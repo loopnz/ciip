98cd31d94ee29991684bc14f71c476f5ffc05e7e
cp <286420375@qq.com>
2017-05-18 17:22:47 +0800

    modify gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -17,18 +17,6 @@ var yeoman = {
 var paths = {
   scripts: [yeoman.app + '/scripts/**/*.js'],
   styles: [yeoman.app + '/styles/**/*.scss'],
-  test: ['test/spec/**/*.js'],
-  testRequire: [
-    yeoman.app + '/bower_components/angular/angular.js',
-    yeoman.app + '/bower_components/angular-mocks/angular-mocks.js',
-    yeoman.app + '/bower_components/angular-resource/angular-resource.js',
-    yeoman.app + '/bower_components/angular-cookies/angular-cookies.js',
-    yeoman.app + '/bower_components/angular-sanitize/angular-sanitize.js',
-    yeoman.app + '/bower_components/angular-ui-router/release/angular-ui-router.js',
-    'test/mock/**/*.js',
-    'test/spec/**/*.js'
-  ],
-  karma: 'karma.conf.js',
   views: {
     main: yeoman.app + '/index.html',
     files: [yeoman.app + '/views/**/*.html']
@@ -48,7 +36,7 @@ var styles = lazypipe()
     outputStyle: 'expanded',
     precision: 10
   })
-  .pipe($.autoprefixer, 'last 1 version')
+  .pipe($.autoprefixer, 'last 5 version')
   .pipe(gulp.dest, '.tmp/styles');
 
 ///////////
@@ -130,14 +118,6 @@ gulp.task('serve:prod', function() {
   });
 });
 
-gulp.task('test', ['start:server:test'], function () {
-  var testToFiles = paths.testRequire.concat(paths.scripts, paths.test);
-  return gulp.src(testToFiles)
-    .pipe($.karma({
-      configFile: paths.karma,
-      action: 'watch'
-    }));
-});
 
 // inject bower components
 gulp.task('bower', function () {
@@ -160,10 +140,10 @@ gulp.task('clean:dist', function (cb) {
 gulp.task('client:build', ['html', 'styles'], function () {
   var jsFilter = $.filter('**/*.js');
   var cssFilter = $.filter('**/*.css');
-  var indexHtmlFilter = $.filter(['**/*', '!**/index.html']);
+  var indexHtmlFilter = $.filter(['**/*', '!**/index.html'],{restore:true});
 
   return gulp.src(paths.views.main)
-    .pipe($.useref({searchPath: [yeoman.app, '.tmp']}))
+    .pipe($.useref({searchPath: [yeoman.app, '.tmp','bower_components']}))
     .pipe(jsFilter)
     .pipe($.ngAnnotate())
     .pipe($.uglify())
